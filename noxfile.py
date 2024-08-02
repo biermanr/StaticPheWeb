cfb354a21616805c1b6f81da1daf21dc8ad78085
Rob Bierman <rbierman@princeton.edu>
2024-08-02 11:08:28 -0400

    chore: Add basic tests and noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,12 @@
+"""noxfile.py to test locally."""
+
+import nox
+
+
+# python="3.10" is failing with "RuntimeError: failed to find interpreter for Builtin discover of python_spec='python3.10'"
+@nox.session(python=["3.8", "3.9", "3.11", "3.12"])
+def tests(session):
+    """Test install and pytest."""
+    session.install(".")
+    session.install("pytest")
+    session.run("pytest")
---
+++ test/test_cli.py
@@ -0,0 +1,20 @@
+"""Tests for spheweb CLI."""
+
+from click.testing import CliRunner
+from spheweb import __version__
+from spheweb.cli import spheweb
+
+runner = CliRunner()
+
+
+def test_version():
+    """Test the version command."""
+    result = runner.invoke(spheweb, ["--version"])
+    assert result.exit_code == 0
+    assert result.output == f"spheweb, version {__version__}\n"
+
+
+def test_parse_help():
+    """Test the help message for the parse command."""
+    result = runner.invoke(spheweb, ["parse", "--help"])
+    assert result.exit_code == 0
