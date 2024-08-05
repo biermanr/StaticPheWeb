d63f578c0a7467b61c113c1e10b9eb2a03a59778
Rob Bierman <rbierman@princeton.edu>
2024-08-05 10:30:09 -0400

    chore: setup pytest/pytest-cov for nox

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,5 @@ import nox
 @nox.session(python=["3.8", "3.9", "3.11", "3.12"])
 def tests(session):
     """Test install and pytest."""
-    session.install(".")
-    session.install("pytest")
-    session.run("pytest")
+    session.install(".", "pytest-cov", "pytest")
+    session.run("pytest", "--cov")
