a468abd4de42238a18610cf0335e80ae9809035a
Rob Bierman <rbierman@princeton.edu>
2024-08-07 17:08:28 -0400

    feat: Add template files to package-data (failing)

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import nox
 
 
 # python="3.10" is failing with "RuntimeError: failed to find interpreter for Builtin discover of python_spec='python3.10'"
-@nox.session(python=["3.8", "3.9", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.11", "3.12"])
 def tests(session):
     """Test install and pytest."""
     session.install(".", "pytest-cov", "pytest")
---
+++ test/test_package_data.py
@@ -0,0 +1,15 @@
+"""Tests for the package data."""
+
+import importlib.resources
+
+
+def test_existing_html():
+    """Test that the template html is present in the package-data."""
+    p = importlib.resources.files("spheweb.templates").joinpath("template.html")
+    assert p.is_file()
+
+
+def test_missing_html():
+    """Test that the missing template is not present, should fail."""
+    x = importlib.resources.files("spheweb.templates").joinpath("missing_template.html")
+    assert x.is_file()
